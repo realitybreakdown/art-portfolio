fca5ecd134f6547ee669f5ba17f78f4a81090825
Ryan <ryanarchambault1@gmailcom>
2018-12-19 14:03:31 -0800

    git lfs add images and mp4

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -4,11 +4,36 @@ import './Video.css';
 
 const Video = (props) => {
   return (
-    <div className="test">
+    <div className="Video">
         <Player
           playsInline
-          poster="https://i.ytimg.com/vi/C0DPdy98e4c/hqdefault.jpg"
-          src="https://youtu.be/C0DPdy98e4c"
+          poster="https://i.imgur.com/2lUXy5u.jpg"
+          src="../../img/Video/Newt.mp4"
+        />
+        <Player
+          playsInline
+          poster="https://i.imgur.com/iBpuFbB.png"
+          src="../../img/Video/NothingNew1.mp4"
+        />
+        <Player
+          playsInline
+          poster="https://i.imgur.com/sbhmc3G.png"
+          src="../../img/Video/NothingNew2.mp4"
+        />
+        <Player
+          playsInline
+          poster="https://i.imgur.com/d5HYi5l.png"
+          src="../../img/Video/NothingNew3.mp4"
+        />
+        <Player
+          playsInline
+          poster="https://i.imgur.com/SaTvSWu.png"
+          src="../../img/Video/NothingNew4.mp4"
+        />
+        <Player
+          playsInline
+          poster="https://i.imgur.com/11uLPV7.png"
+          src="../../img/Video/NothingNew5.mp4"
         />
     </div>
   );
