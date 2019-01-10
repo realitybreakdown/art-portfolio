298e4ead313f4142aca49e6b8315024903b7a2f1
Ryan <ryanarchambault1@gmailcom>
2019-01-09 16:17:55 -0800

    add about and info

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './About.css';
 
 class About extends Component {
   render() {
@@ -8,9 +9,47 @@ class About extends Component {
         <p>BFA from California Institute of the Arts</p>
         <br/>
         <h4>Exhibits</h4>
-        <p></p>
+        <pre><b>Hey How Are You Good How Are You Good</b> 2018<br /> 
+		        Various Paintings and Mixed Media<br />  
+            D301, California Institute of the Arts Valencia, California<br /> 
+            <br /> 
+			      <b>Welcome Home</b> 2017<br /> 
+            “Vase” Painting<br /> 
+            “Spittoon 1-3” Sculpture<br /> 
+            “Couch” Sculpture<br />  
+            “Body” Painting<br /> 
+            “Inside” Painting<br />  
+            “Picnic” Mixed Media<br /> 
+            “Snake” Relief<br />  
+            D301, California Institute of the Arts Valencia, California<br /> 
+            <br />
+            <b>Accidentally On Purpose</b> Group Exhibition 2017<br />
+                PANEL LA, Los Angeles<br />
+                "Stool" Sculpture<br />
+            <br />  
+            <b>Blinkers</b> 2017<br />  
+                Glasgow, UK<br /> 
+                <br /> 
+            <b>Nothing New Series</b> 2016<br /> 
+                Video Screening<br />  					
+			          Stevenson Blanch, Valencia California<br />  
+                <br /> 
+            <b>Group Exhibition</b> 2015<br /> 
+              “Vase” Painting<br /> 
+              “Landscape” Painting<br />  
+              “Spittoon” Sculpture<br /> 
+              Main Gallery, California Institute of the Arts Valencia, California<br />  
+              <br /> 
+            <b>Group Exhibition</b> 2015<br /> 
+                “Inconsequential Memory Series” Paintings<br />  
+                Main Gallery, California Institute of the Arts, Valencia, California<br />   
+                <br /> 
+            <b>Chiquita Landfill Scholarship Competition</b> 2015<br /> 
+                “Quilt” Sculpture<br />  
+                Main Gallery, California Institute of the Arts, Valencia, California<br />    
+        </pre>
         <br/>
-        <p>ryanarchambault1@gmail.com</p>
+        <p className="Contact">ryanarchambault1@gmail.com</p>
       </div>
     );
   }
--- src/components/Paintings/Paintings.jsx
+++ src/components/Paintings/Paintings.jsx
@@ -6,11 +6,26 @@ class Paintings extends Component {
     return (
       <div className="Paintings">
         <div className="PaintingsImg">
-          <img src="../../img/Paintings/Desert.png" alt="Desert"/>
-          <img src="../../img/Paintings/HandInGrass.png" alt="Grass"/>
-          <img src="../../img/Paintings/Oil.png" alt="Oil"/>
-          <img src="../../img/Paintings/Picnic.png" alt="Picnic"/>
-          <img src="../../img/Paintings/Toothbrush.png" alt="Toothbrush"/>
+          <div className="Paint">
+            <img src="../../img/Paintings/Desert.png" alt="Desert"/>
+            <p><i>Desert</i> 2015, Acrylic</p>
+          </div>
+          <div className="Paint">
+            <img src="../../img/Paintings/HandInGrass.png" alt="Grass"/>
+            <p><i>Hand</i> 2015, Astro Turf, Acrylic, Wood, Metal Wire</p>
+          </div>
+          <div className="Paint">
+            <img src="../../img/Paintings/Oil.png" alt="Oil"/>
+            <p><i>Oil Spill</i> 2015, Oil paint, Shop Rag</p>
+          </div>
+          <div className="Paint">
+            <img src="../../img/Paintings/Picnic.png" alt="Picnic"/>
+            <p><i>Picnic</i> 2016, Acrylic, Yarn</p>
+          </div>
+          <div className="Paint">
+            <img src="../../img/Paintings/Toothbrush.png" alt="Toothbrush"/>
+            <p><i>Toothbrush</i> 2015, Acrylic, Laminate, Wood, Metal Wire</p>
+          </div>
         </div>
       </div>
     );
